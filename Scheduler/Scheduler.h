5ae346290781b84f2edf7366b56a4b46a02b615f
gomi_ningen <korun24276@gmail.com>
2016-10-15 17:59:10 +0900

    Initial commit

---
+++ Scheduler/Scheduler.h
@@ -0,0 +1,18 @@
+//
+//  Scheduler.h
+//  Scheduler
+//
+//  Copyright © 2016年 gomi_ningen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Scheduler.
+FOUNDATION_EXPORT double SchedulerVersionNumber;
+
+//! Project version string for Scheduler.
+FOUNDATION_EXPORT const unsigned char SchedulerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scheduler/PublicHeader.h>
+
+
